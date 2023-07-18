4121bae5d76289c103ed382f02bb6231f77db254
Murillo Marques <murillo.marques.it@gmail.com>
2023-07-18 18:18:10 +0000

    result of mob programming

--- src/test/java/org/rockpaperscissor/MainTest.java
+++ src/test/java/org/rockpaperscissor/MainTest.java
@@ -4,22 +4,38 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Objects;
+
 class MainTest {
 
-    private void rockPaperScissor(){
+    private boolean rockPaperScissor(String weapon1, String weapon2){
+        if (weapon1.contains("rock") && weapon2.contains("scissors")){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
 
+       //return Objects.equals(weapon1, weapon2);        
     }
 
 
-    @Test
-    void testRockPaperScissor() {
-        rockPaperScissor();
-        assertTrue(true);
-    }
+   @Test
+   void testrock_rock(){
+        rockPaperScissor("rock","rock");
+        assertTrue(true);    
+   }
+   
+   @Test
+   void testRockSmashScissors(){
+    boolean result = rockPaperScissor("rock", "scissors");
+    assertTrue(result);
+   }
 
-    @Test
-    void testFailedRockPaperScissor() {
-        rockPaperScissor();
-        assertTrue(false);
-    }
+   @Test
+   void testScissorsLosesToRock(){
+    boolean result = rockPaperScissor("scissors", "rock");
+    assertFalse(result);
+   }
 }
